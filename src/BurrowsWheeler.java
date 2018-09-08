b0e98160480cdf6e8a77c387416b0a501d37ec0b
Jack <jyhc40@gmail.com>
2018-09-07 17:44:11 -0700

    Transform done

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -7,10 +7,14 @@ public class BurrowsWheeler {
         String s = StdIn.readString();
         CircularSuffixArray csa = new CircularSuffixArray(s);
         StringBuilder builder = new StringBuilder(csa.length());
+        char temp;
         for(int i = 0; i < csa.length(); i++) {
-            if(csa.index(i) == 0) StdOut.print(i);
-            StdOut.print(+ "\n");
+            if(csa.index(i) == 0) StdOut.print(i+"\n");
+            if(csa.index(i)-1 < 0) temp = s.charAt(s.length()-1);
+            else temp = s.charAt(csa.index(i)-1);
+            builder.append(temp);
         }
+        StdOut.print(builder.toString());
     }
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
@@ -21,6 +25,8 @@ public class BurrowsWheeler {
     // if args[0] is '-', apply Burrows-Wheeler transform
     // if args[0] is '+', apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-        
+        if(args[0].contentEquals("-")) {
+            BurrowsWheeler.transform();
+        }
     }
 }
